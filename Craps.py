ea0350efeddd459e36c25ef7cb9712f4411823c8
Bernardo Cunha Capoferri <bernardocc@al.insper.edu.br>
2020-04-02 09:38:03 -0300

    Fix retorno do Pass line bet

--- Craps.py
+++ Craps.py
@@ -26,13 +26,13 @@ def comeco_jogo():
                 print('Valor inválido por favor insira um valor válido.')
                 continue
             creditos = Pass_Line_Bet(creditos, aposta_plb)
-            print('Você tem {} fichas'.format(creditos))
 # Função que simula Pass Line Bet
 def Pass_Line_Bet(creditos, aposta_plb):
 # lance e soma dos dados 
     dado1 = randint(1, 6)
     dado2 = randint(1, 6)
     soma_dados = dado1 + dado2
+    print('O lance dos dados deu: {}'.format(soma_dados))
     if soma_dados == 7 or soma_dados == 11:
         creditos += aposta_plb
         print('Parabéns, você ganhou!')
@@ -40,15 +40,15 @@ def Pass_Line_Bet(creditos, aposta_plb):
         creditos -= aposta_plb
         print('Você perdeu.')
     else:
-        comeOut = False
         falta_de_conclusao = True
         point = soma_dados
         print('Fase: "Point"')
-# Fase Point
         while falta_de_conclusao:
+# Fase Point
             dado1 = randint(1,6)
             dado2 = randint(1,6)
             soma_dados = dado1 + dado2
+            print('O lance dos dados deu: {}'.format(soma_dados))
             if point == soma_dados:
                 creditos += aposta_plb
                 print('Você ganhou!')
@@ -57,7 +57,7 @@ def Pass_Line_Bet(creditos, aposta_plb):
                 creditos -= aposta_plb
                 print('Você perdeu.')
                 falta_de_conclusao = False
-        return creditos
+    return creditos
 
 
     
